3aa2a1a6212cb70e8f28031f49c71ccb5ef8d33e
mayankaryaca <mayankaryaca@gmail.com>
2021-05-19 12:35:27 -0400

    Firebase parking + profile controllers changed + on click detail added

---
+++ ParkingProject/View/ParkingDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ParkingDetailsViewController.swift
+//  ParkingProject
+//
+//  Created by Mayank Arya on 2021-05-19.
+//
+
+import UIKit
+
+class ParkingDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
